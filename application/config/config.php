764e854d5885ac1caf632e5b8e0bcceb1c254a69
Arginanta <arginanta30@gmail.com>
2025-02-14 15:20:32 +0700

    Kolom Halaman Register

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://localhost/weecom';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
---
+++ application/views/register.php
@@ -0,0 +1,19 @@
+<div class="container">
+  <div class="row">
+
+    <div class="col-md-6">
+      <h2>Sistem Perusahaan Weecom</h2>
+      <h3>Pengelolaan Karyawan & Digital Absensi</h3>
+
+      <div class="akses-button">
+        <?php
+
+        echo anchor('user/login', 'LOGIN', ['class' => 'btn btn-outline-primary']);
+        echo anchor('user/register', 'REGISTER', ['class' => 'btn btn-outline-primary']);
+
+        ?>
+      </div>
+    </div>
+
+  </div>
+</div>
